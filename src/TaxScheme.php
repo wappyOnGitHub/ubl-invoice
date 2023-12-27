8651c1642b2a3396477a03af618391805a4e93fd
wappy-yoga <floreac@gmail.com>
2023-12-27 13:13:04 +0200

    TaxScheme - make ID optional

--- src/TaxScheme.php
+++ src/TaxScheme.php
@@ -92,9 +92,11 @@ class TaxScheme implements XmlSerializable
      */
     public function xmlSerialize(Writer $writer): void
     {
-        $writer->write([
-            Schema::CBC . 'ID' => $this->id
-        ]);
+        if ($this->id !== null) {
+            $writer->write([
+                Schema::CBC . 'ID' => $this->id
+            ]);
+        }
         if ($this->name !== null) {
             $writer->write([
                 Schema::CBC . 'Name' => $this->name
